c332b8b359550596d43ef31c05a995d93a5e42d7
unknown <stefanie.zwingenberger03@htl-salzburg.ac.at>
2020-06-09 16:14:28 +0200

    Newest Upload

---
+++ Assets/Mygame/Scenes/wormController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wormController : MonoBehaviour
+{
+    public KeyCode jumpKey;
+    public KeyCode fowardKey;
+    public KeyCode shootKey;
+    public GameObject Projektil;
+    public Rigidbody z;
+    public Quaternion Quat;
+    public Vector3 w;
+    public Vector3 x;
+    public Vector3 y;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(jumpKey))
+        {
+            Debug.Log("Jump taste wurde gedrückt");
+            z.AddForce(x);
+        }
+
+        if (Input.GetKeyDown(fowardKey))
+        {
+            Debug.Log("Forwärts taste wurde gedrückt");
+            z.AddForce(y);
+
+        }
+
+        if (Input.GetKeyDown(shootKey))
+        {
+            Debug.Log("Schiessen wenn taste gedrückt wurde");
+            Instantiate(Projektil,w,Quat);
+            
+        }
+
+
+    }
+}
